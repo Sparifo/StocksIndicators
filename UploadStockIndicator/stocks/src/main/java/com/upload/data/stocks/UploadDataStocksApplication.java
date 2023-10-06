5ff275c05b419477279660856818ad1ab98c9630
Mateusz Miernik <mat.miernik@hotmail.com>
2023-10-06 23:15:22 +0200

    Init UploadStockIndicators

---
+++ UploadStockIndicator/stocks/src/main/java/com/upload/data/stocks/UploadDataStocksApplication.java
@@ -0,0 +1,13 @@
+package com.upload.data.stocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDataStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadDataStocksApplication.class, args);
+	}
+
+}
---
+++ UploadStockIndicator/stocks/src/test/java/com/upload/data/stocks/UploadDataStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upload.data.stocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadDataStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
